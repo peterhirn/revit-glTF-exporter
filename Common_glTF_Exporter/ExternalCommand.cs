acc2834af08fbb6316a719765b7ab3d04dc8dd52
Peter Hirn <peter.hirn@phi.ag>
2026-03-25 09:56:00 +0100

    feat: export kinship folder

--- Common_glTF_Exporter/ExternalCommand.cs
+++ Common_glTF_Exporter/ExternalCommand.cs
@@ -9,9 +9,11 @@
     using Microsoft.Win32;
     using Newtonsoft.Json;
     using System;
+    using System.Collections.Generic;
     using System.Collections.Immutable;
     using System.IO;
     using System.Text;
+    using System.Text.RegularExpressions;
 
     static class Export
     {
@@ -166,7 +168,7 @@
                 var sb = new StringBuilder();
                 sb.AppendLine($"Symbols: {symbols.Count.ToString()}");
 
-                foreach(var item in symbols)
+                foreach (var item in symbols)
                 {
                     sb.AppendLine(item.Family.Name);
                 }
@@ -334,6 +336,126 @@
 
                         var path = Path.Combine(target, doc.Title);
                         Export.View(doc, view, path);
+                    }
+                    catch (Exception ex)
+                    {
+                        TaskDialog.Show("Error", doc.Title + Environment.NewLine + ex.ToString());
+                    }
+                    finally
+                    {
+                        doc.Close(false);
+                    }
+                }
+
+                var serialized = JsonConvert.SerializeObject(mapping,
+                    new JsonSerializerSettings { NullValueHandling = NullValueHandling.Ignore, Formatting = Formatting.Indented });
+
+                var mappingFile = Path.Combine(target, "mapping.json");
+                File.WriteAllText(mappingFile, serialized);
+
+                TaskDialog.Show("Export folder", "Done");
+
+                return Result.Succeeded;
+            }
+            catch (Exception ex)
+            {
+                TaskDialog.Show("Error", ex.ToString());
+                return Result.Failed;
+            }
+        }
+    }
+
+    [Transaction(TransactionMode.Manual)]
+    [Regeneration(RegenerationOption.Manual)]
+    public class ExportKinshipFolder : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            try
+            {
+                var uiapp = commandData.Application;
+                var app = uiapp.Application;
+
+                var dialog = new OpenFolderDialog();
+                var result = dialog.ShowDialog();
+
+                if (result is null || result == false)
+                {
+                    return Result.Cancelled;
+                }
+
+                var target = Path.Combine(dialog.FolderName, "out");
+                Directory.CreateDirectory(target);
+
+                using var log = new StreamWriter(Path.Combine(target, "log.txt"));
+
+                var versionDirectories =
+                    Directory.GetDirectories(dialog.FolderName)
+                    .Where(d => Regex.IsMatch(Path.GetFileName(d)!, @"^\d{4}$"))
+                    .OrderDescending();
+
+                /// NOTE: dedup, prefer latest Revit version
+                var familyDirectories = new Dictionary<string, string>();
+                foreach (var versionDirectory in versionDirectories)
+                {
+                    foreach (var path in Directory.GetDirectories(versionDirectory))
+                    {
+                        familyDirectories.TryAdd(Path.GetFileName(path), path);
+                    }
+                }
+
+                var families = new List<string>();
+                foreach (var familyDirectory in familyDirectories.Values)
+                {
+                    families.AddRange(
+                        Directory.GetFiles(familyDirectory)
+                        .Where(f => Path.GetExtension(f).ToLowerInvariant() == ".rfa"));
+                }
+
+                TaskDialog.Show("Kinship Families", $"Exporting {families.Count} families");
+
+
+                var mapping = new Dictionary<string, string>();
+
+                foreach (var file in families)
+                {
+                    var doc = app.OpenDocumentFile(file);
+                    try
+                    {
+                        if (!doc.IsFamilyDocument) throw new InvalidOperationException("Document is not a family");
+                        if (doc.FamilyManager is null) throw new InvalidOperationException("FamilyManager is null");
+
+                        using var stencilIdParameter =
+                            doc.FamilyManager.Parameters.Cast<FamilyParameter>().FirstOrDefault(p => p.Definition.Name == "06 Stencil ID")
+                            ?? throw new InvalidOperationException("Missing StencilId parameter");
+
+                        var stencilIds = doc.FamilyManager.Types.Cast<FamilyType>().Select(type => type.AsString(stencilIdParameter)).Distinct();
+                        foreach (var stencilId in stencilIds)
+                        {
+                            if (!mapping.TryAdd(stencilId, doc.Title))
+                            {
+                                log.WriteLine($"Duplicated stencil id {stencilId} {doc.Title} {mapping[stencilId]}");
+                            }
+                        }
+
+                        using var collector = new FilteredElementCollector(doc).OfClass(typeof(View3D));
+                        using var view = collector.Cast<View3D>().FirstOrDefault() ?? throw new InvalidOperationException("Missing 3d view");
+
+                        var serviceArea = doc.Settings.Categories.get_Item(BuiltInCategory.OST_SpecialityEquipment)?.SubCategories.Cast<Category>().FirstOrDefault(c => c.Name == "Working & Service Area");
+                        if (serviceArea is not null)
+                        {
+                            using var tx = new Transaction(doc, "Hide service area");
+                            tx.Start();
+                            view.SetCategoryHidden(serviceArea.Id, true);
+                            tx.Commit();
+                        }
+
+                        var path = Path.Combine(target, doc.Title);
+                        Export.View(doc, view, path);
+                    }
+                    catch (Exception ex)
+                    {
+                        log.WriteLine($"Exception {doc.Title} {ex.Message}");
                     }
                     finally
                     {
